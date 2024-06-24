71b7337aa65f5f8757d65b1968211edb70f36992
Mashrafi Rahman <monomycelium@icloud.com>
2024-06-24 14:32:07 +0800

    Test argument parsing using zig-clap.

--- build.zig
+++ build.zig
@@ -40,6 +40,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const clap = b.dependency("clap", .{});
+    exe.root_module.addImport("clap", clap.module("clap"));
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,42 @@
 const std = @import("std");
+const clap = @import("clap");
+const log = std.log;
+
+const debug = std.debug;
+const io = std.io;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const alloc = std.heap.raw_c_allocator;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help            Display this help and exit.
+        \\-r, --rails <usize>   Number of rails (key).
+        \\-d, --decode          Decode data instead of encoding it.
+        \\<file>...
+        \\
+    );
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const parsers = comptime .{
+        .file = clap.parsers.string,
+        .usize = clap.parsers.int(usize, 10),
+    };
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, parsers, .{
+        .diagnostic = &diag,
+        .allocator = alloc,
+    }) catch |err| {
+        diag.report(io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
 
-    try bw.flush(); // don't forget to flush!
+    if (res.args.help != 0 or res.args.rails == null or res.positionals.len == 0)
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+
+    std.debug.print("rails: {}\n", .{res.args.rails.?});
+    std.debug.print("files to {s}: ", .{if (res.args.decode == 0) "encode" else "decode"});
+    for (res.positionals, 0..) |arg, i|
+        std.debug.print("{s}{s}", .{arg, if (i == res.positionals.len - 1) "\n" else ", "});
 }
 
 test "simple test" {
